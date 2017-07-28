63654325068e198da0caa2aca855689f7088598a
楼晨 <louchen@iishang.com>
2017-07-28 09:57:42 +0800

    增加一个principal接口

--- src/main/java/com/springbootvue/api/auth/AuthController.java
+++ src/main/java/com/springbootvue/api/auth/AuthController.java
@@ -3,19 +3,22 @@ package com.springbootvue.api.auth;
 import com.springbootvue.api.user.model.User;
 import com.springbootvue.secruity.JwtAuthenticationRequest;
 import com.springbootvue.secruity.JwtAuthenticationResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 
 @RestController
+@Slf4j
 public class AuthController {
     @Value("${jwt.header}")
     private String tokenHeader;
@@ -23,29 +26,34 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
-    @RequestMapping(value = "${jwt.route.authentication.path}", method = RequestMethod.POST)
-    public ResponseEntity<?> createAuthenticationToken(
-            @RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException{
-        final String token = authService.login(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    @PostMapping(value = "${jwt.route.authentication.register}")
+    public User register(@RequestBody User addedUser) throws AuthenticationException {
+        return authService.register(addedUser);
+    }
 
+    @PostMapping(value = "${jwt.route.authentication.path}")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtAuthenticationRequest authenticationRequest) throws AuthenticationException {
+        final String token = authService.login(authenticationRequest.getUsername(), authenticationRequest.getPassword());
         // Return the token
         return ResponseEntity.ok(new JwtAuthenticationResponse(token));
     }
 
-    @RequestMapping(value = "${jwt.route.authentication.refresh}", method = RequestMethod.GET)
-    public ResponseEntity<?> refreshAndGetAuthenticationToken(
-            HttpServletRequest request) throws AuthenticationException{
+    @GetMapping(value = "${jwt.route.authentication.refresh}")
+    public ResponseEntity<?> refreshAndGetAuthenticationToken(HttpServletRequest request) throws AuthenticationException {
         String token = request.getHeader(tokenHeader);
         String refreshedToken = authService.refresh(token);
-        if(refreshedToken == null) {
+        if (refreshedToken == null) {
             return ResponseEntity.badRequest().body(null);
         } else {
             return ResponseEntity.ok(new JwtAuthenticationResponse(refreshedToken));
         }
     }
 
-    @RequestMapping(value = "${jwt.route.authentication.register}", method = RequestMethod.POST)
-    public User register(@RequestBody User addedUser) throws AuthenticationException{
-        return authService.register(addedUser);
+    @GetMapping(value = "${jwt.route.authentication.principal}")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> getPrincipal(HttpServletRequest request){
+        Principal principal = request.getUserPrincipal();
+        return ResponseEntity.ok(principal);
     }
+
 }
--- src/test/java/com/springbootvue/test/ChannelTest.java
+++ src/test/java/com/springbootvue/test/ChannelTest.java
@@ -2,15 +2,9 @@ package com.springbootvue.test;
 
 import com.springbootvue.api.channel.model.Channel;
 import com.springbootvue.api.channel.repository.ChannelRepository;
-import com.springbootvue.api.user.model.User;
-import com.springbootvue.api.user.repository.UserRepository;
 import com.test.base.BaseTest;
-import com.test.base.RandomMock;
-import org.apache.commons.lang3.RandomUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.Map;
 
 /**
  * Created by louchen on 2017/7/17.
@@ -23,7 +17,7 @@ public class ChannelTest extends BaseTest {
     @Test
     public void create(){
         Channel channel = new Channel();
-        channel.setName("abc"+ RandomUtils.nextInt(1,100));
+        channel.setName("abc");
         channelRepository.save(channel);
     }
 
